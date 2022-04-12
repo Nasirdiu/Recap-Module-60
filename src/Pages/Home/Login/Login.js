edfc5f9dd3369433052d0002a4f9ae9c1be78db8
nasirdiu <nasir.joypurhat@gmail.com>
2022-04-12 17:39:36 +0600

    login

--- src/Pages/Home/Login/Login.js
+++ src/Pages/Home/Login/Login.js
@@ -1,23 +1,34 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+
+    const handleSubmit=event=>{
+        event.preventDefault();
+    }
   return (
     <div className="container bg-success d-flex justify-content-center mt-3 w-75">
-      <Form className="w-25">
+      <Form onSubmit={handleSubmit} className="w-25">
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" required />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" required/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Forget Password" />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <p>
+          AllReady Have and account?
+          <Link className="from-link" to="/login">
+            Login
+          </Link>
+        </p>
+        <Button variant="primary mb-3" type="submit">
           Submit
         </Button>
       </Form>
